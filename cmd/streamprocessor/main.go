361bf59c17fa4bdf0ca3dd8ca4c9e193dcacb6ac
Zhu <zhusonglai@Live.cn>
2025-10-28 22:32:18 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/streamprocessor/main.go
@@ -0,0 +1,23 @@
+// cmd/streamprocessor/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "streamprocessor/internal/streamprocessor"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := streamprocessor.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
